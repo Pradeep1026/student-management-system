64d78e81cf73b4146048a2309388faf691bc4121
Aditya <aditya9640@gmail.com>
2018-01-23 00:02:02 +0530

    Updated index.php

--- index.php
+++ index.php
@@ -1,40 +1,46 @@
 <!DOCTYPE html>
 <html>
-  <head>
+
+<head>
     <meta charset="utf-8">
     <title>Welcome to student Management system</title>
-  </head>
-  <body>
-    <h5  align="right"> <a href="login.php">Login admin page</a> </h5>
-    <h1 align="center">Welcome to Student Management System</h1>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+   <div class="main_header_top">
+    <h5 align="right"> <a href="login.php">Login admin page</a> </h5>
+    <h1 align="center"><marquee align="right">Welcome to Student Management System</marquee></h1></div>
 
     <form action="index.php" method="post">
-      <table align="center" border="1" style="width:50%">
-        <tr>
-          <td colspan="2" align="center">Student Information</td>
-        </tr>
-        <tr>
-          <td>standard</td>
-          <td>
-            <select name="standard" >
+        <table align="center" border="1" style="width:50%">
+            <tr>
+                <td colspan="2" align="center">Student Information</td>
+            </tr>
+            <tr>
+                <td>standard</td>
+                <td>
+                    <select name="standard">
               <option value="cse">CSE</option>
               <option value="civil">CIVIL</option>
               <option value="mechnical">MECHNICAL</option>
             </select>
-         </td>
-        </tr>
-        <tr>
-          <td>Roll no. </td>
-          <td> <input type="text" name="rollno" required> </input> </td>
-        </tr>
-        <tr>
-          <td colspan="2" align="center"><input type="submit" name="submit" value="show info"></input></td>
-        </tr>
-      </table>
+                </td>
+            </tr>
+            <tr>
+                <td>Roll no. </td>
+                <td> <input type="text" name="rollno" required> </input>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2" align="center"><input type="submit" name="submit" value="show info"></input>
+                </td>
+            </tr>
+        </table>
 
-<!-- showing student details table  -->
-     
-  <?php
+        <!-- showing student details table  -->
+
+        <?php
 
 if (isset($_POST['submit'])) {
   include('dbconnection.php');
@@ -48,10 +54,11 @@ if (isset($_POST['submit'])) {
   if ($row < 1) {
 
       ?>
-      <script type="text/javascript">
-        alert('No student exist with this Roll number');
-      </script>
-      <?php
+            <script type="text/javascript">
+                alert('No student exist with this Roll number');
+
+            </script>
+            <?php
 
   }
   else {
@@ -60,34 +67,45 @@ if (isset($_POST['submit'])) {
     $data = mysqli_fetch_assoc($run)
     
       ?>
- <table border="1" align="center" width="80%" style = "margin-top: 20px">
-  
-    <tr>
-      <th>Roll no.</th>
-      <th>Name</th>
-      <th>City</th>
-      <th>Parent Contact</th>
-      <th>Standard</th>
-      <th>Image</th>
-    </tr>
-    <tr align="center">
-      <td> <?php echo $data['rollno']; ?></td>
-      <td><?php echo $data['name']; ?></td>
-      <td><?php echo $data['city']; ?></td>
-      <td><?php echo $data['pcontact']; ?></td>
-      <td><?php echo $data['standard']; ?></td>
-      <td><img src="dataimg/<?php echo $data['image']; ?>" width="110px" height="100px" ></td>
-     </tr>
+                <table border="1" align="center" width="80%" style="margin-top: 20px">
 
-      <?php
+                    <tr>
+                        <th>Roll no.</th>
+                        <th>Name</th>
+                        <th>City</th>
+                        <th>Parent Contact</th>
+                        <th>Standard</th>
+                        <th>Image</th>
+                    </tr>
+                    <tr align="center">
+                        <td>
+                            <?php echo $data['rollno']; ?>
+                        </td>
+                        <td>
+                            <?php echo $data['name']; ?>
+                        </td>
+                        <td>
+                            <?php echo $data['city']; ?>
+                        </td>
+                        <td>
+                            <?php echo $data['pcontact']; ?>
+                        </td>
+                        <td>
+                            <?php echo $data['standard']; ?>
+                        </td>
+                        <td><img src="dataimg/<?php echo $data['image']; ?>" width="110px" height="100px"></td>
+                    </tr>
+
+                    <?php
   }
     
 }
 
  ?>
-</table>
+                </table>
 
     </form>
 
-  </body>
+</body>
+
 </html>
